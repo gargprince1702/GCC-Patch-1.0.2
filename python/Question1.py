6804f2700f120f2d172943b40d493b507306e637
Prince Garg <54926566+gargprince1702@users.noreply.github.com>
2019-10-19 12:46:45 -0700

    solutionbygargprince1702.py

--- python/Question1.py
+++ python/Question1.py
@@ -1,5 +1,15 @@
 # modify this function, and create other functions below as you wish
 def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
     # modify and then return the variable below
-    answer = -5
+    repayment = repaymentPercentage * initialLevelOfDebt / 100
+    LevelOfDebt = initialLevelOfDebt
+    answer=0
+    while LevelOfDebt*(1+(interestPercentage/100)) >= repayment :
+        LevelOfDebt = LevelOfDebt*(1+(interestPercentage/100)) - repayment
+        answer = answer + repayment
+    if LevelOfDebt*(1+(interestPercentage/100)) < repayment and LevelOfDebt*(1+(interestPercentage/100)) > 50:
+        answer = answer+ LevelOfDebt*(1+(interestPercentage/100))
+    elif LevelOfDebt*(1+(interestPercentage/100)) <= 50:
+        answer = answer + 50
+    answer = answer + repayment
     return answer
